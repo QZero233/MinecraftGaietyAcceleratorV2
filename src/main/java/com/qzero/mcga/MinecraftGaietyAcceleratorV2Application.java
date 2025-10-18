8b09f381cfc6e76d835dd4a934636d860c62f879
QZero <51401792+QZero233@users.noreply.github.com>
2025-10-18 15:13:19 +0800

    Init commit

---
+++ src/main/java/com/qzero/mcga/MinecraftGaietyAcceleratorV2Application.java
@@ -0,0 +1,13 @@
+package com.qzero.mcga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinecraftGaietyAcceleratorV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinecraftGaietyAcceleratorV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/qzero/mcga/MinecraftGaietyAcceleratorV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qzero.mcga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinecraftGaietyAcceleratorV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
